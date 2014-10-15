73908188d572851d12b00bb80ab6eb0307d167ce
Dmidd87 <dmidd87@gmail.com>
2014-10-15 13:40:14 -0600

    refactored quotes into the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
 class PagesController < ApplicationController
 
+  def index
+    @quotes = [
+      ["Failure is not an option", "-Bob"],
+      ["Success please", "-Me"],
+      ["Win everything!", "-Stan"]
+    ]
+  end
+
 end
