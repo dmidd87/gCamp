a229437e275737a53350014b4f45d6cc076e08d4
Dmidd87 <dmidd87@gmail.com>
2014-10-15 14:12:08 -0600

    Turned array of strings into an array of quote objects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,21 @@
 class PagesController < ApplicationController
 
   def index
-    @quotes = [
-      ["Failure is not an option", "-Bob"],
-      ["Success please", "-Me"],
-      ["Win everything!", "-Stan"]
-    ]
+
+
+    quote1 = Quote.new
+    quote1.text = "Failure is not an option"
+    quote1.author = "-Bob"
+
+    quote2 = Quote.new
+    quote2.text = "Success please"
+    quote2.author = "-Me"
+
+    quote3 = Quote.new
+    quote3.text = "Win everything!"
+    quote3.author = "-Stan"
+
+    @quotes = [quote1, quote2, quote3]
   end
 
 end
