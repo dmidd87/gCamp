cff3df4d95d8212ed37525cb7b07a4404c928779
Dmidd87 <dmidd87@gmail.com>
2014-11-06 07:42:35 -0700

    Wrote sign up, sign in, sign out, and users tests

---
+++ spec/features/signin_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+  feature "Sign-in" do
+
+    scenario "User signs up" do
+      visit sign_up_path
+
+    fill_in "First name", with: "David"
+    fill_in "Last name", with: "Example"
+    fill_in "Email address", with: "example@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+      click_on "Register"
+
+    expect(page).to have_content "David"
+      click_on "David"
+
+    expect(page).to have_content "David"
+    expect(page).to have_content "Example"
+    expect(page).to have_content "example@example.com"
+      click_on "Edit"
+
+    expect(page).to have_content "First name"
+    fill_in "First name", with: "Stan"
+      click_on "Update User"
+
+    expect(page).to have_content "Stan"
+      click_on "Back"
+
+    expect(page).to have_content "Stan"
+    expect(page).to have_content "example@example.com"
+      click_on "Sign Out"
+
+    expect(page).to have_no_content "example@example.com"
+      click_on "Sign In"
+
+    fill_in "Email", with: "example@example.com"
+    fill_in "Password", with: "password"
+      click_on "Enter"
+
+    expect(page).to have_content "Stan"
+
+      click_on "Sign Out"
+
+
+
+  end
+
+end
