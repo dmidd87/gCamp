69c0a91dfbfeb5d3fac8ddc92a716766a9d78f1a
Dmidd87 <dmidd87@gmail.com>
2014-11-07 14:08:34 -0700

    refactored user can sign up and sign out tests

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -2,28 +2,27 @@ require 'rails_helper'
 
 feature "User auth" do
 
+  background do
+    @password = "password"
+    @user = User.new(
+      :first_name => "David",
+      :last_name => "Example",
+      :email_address => "example@example.com",
+      :password => @password,
+      :password_confirmation => @password
+    )
+  end
+
   scenario "User can sign up" do
-    visit signup_path
-    fill_in "First name", with: "David"
-    fill_in "Last name", with: "Example"
-    fill_in "Email address", with: "example@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_on "Register"
-    expect(page).to have_content "David"
+    register(@user,@password)
+    expect(page).to have_content @user.first_name
   end
 
   scenario "Users can sign out" do
-    visit signup_path
-    fill_in "First name", with: "David"
-    fill_in "Last name", with: "Example"
-    fill_in "Email address", with: "example@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_on "Register"
-    expect(page).to have_content "David"
+    register(@user,@password)
+    expect(page).to have_content @user.first_name
     click_on "Sign Out"
-    expect(page).to have_no_content "David"
+    expect(page).to have_no_content @user.first_name
   end
 
 end
