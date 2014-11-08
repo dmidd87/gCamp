5f86078e6f4b510ad3fb600b0bc6128dd7a4c89b
Dmidd87 <dmidd87@gmail.com>
2014-11-08 15:38:11 -0700

    refactored tasks spec test

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -5,26 +5,31 @@ feature "Tasks" do
   scenario "User creates a task" do
     visit tasks_path
       click_on "Create Task"
-
       fill_in "Description", with: "My awesome task!"
-        expect(page).to have_no_content "My awesome task!"
       click_on "Create Task"
+      expect(page).to have_content "My awesome task!"
+  end
 
-        expect(page).to have_content "My awesome task!"
+  scenario "User edits a task" do
+    visit tasks_path
+      click_on "Create Task"
+      fill_in "Description", with: "My awesome task!"
+      click_on "Create Task"
+      expect(page).to have_content "My awesome task!"
       click_on "Edit"
-
       fill_in "Description", with: "My edited task!"
       click_on "Update Task"
+      expect(page).to have_content "My edited task!"
+  end
 
-        expect(page).to have_content "My edited task!"
+  scenario "User deletes a task" do
+    visit tasks_path
+      click_on "Create Task"
+      fill_in "Description", with: "My awesome task!"
+      click_on "Create Task"
+      expect(page).to have_content "My awesome task!"
       click_on "Back"
-
-        expect(page).to have_content "My edited task!"
       click_on "Destroy"
-
-        expect(page).to have_no_content "My edited task!"
-
-
- end
-
+      expect(page).to have_no_content "My awesome task!"
+  end
 end
