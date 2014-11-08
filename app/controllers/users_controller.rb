995fd61aad8c9e67a218b0588339f57254aa6209
Dmidd87 <dmidd87@gmail.com>
2014-11-08 16:05:13 -0700

    Fixed create in the controller but it's still not saving to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,16 +30,9 @@
     end
 
     def create
-      @user = User.new(params.require(:user).permit(:first_name, :last_name, :email_address, :password, :password_confirmation))
-      respond_to do |format|
-        if @user.save
-          format.html { redirect_to @user, notice: 'User was successfully created.'}
-          format.json { render :show, status: :created, location: @user }
-        else
-          format.html { render :new }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
-      end
+      @user = User.new(user_params)
+      @user.save
+      redirect_to users_path, notice: 'User was successfully created.'
     end
 
     def destroy
