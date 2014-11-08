9ecee391932f4f3de33e3fb2068858cc64118a2f
Dmidd87 <dmidd87@gmail.com>
2014-11-08 15:52:34 -0700

    Refactored most of user_spec for testing but realized users was broken, fixing now

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -5,66 +5,60 @@ feature "Users" do
   scenario "User creates a user" do
     visit users_path
       click_on "Create User"
-
-    fill_in "First name", with: "David"
-    fill_in "Last name", with: "Example"
-    fill_in "Email address", with: "example@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
+      fill_in "First name", with: "David"
+      fill_in "Last name", with: "Example"
+      fill_in "Email address", with: "example@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
       click_on "Create User"
-
-    expect(page).to have_content "David"
-    expect(page).to have_content "Example"
-    expect(page).to have_content "example@example.com"
-      click_on "Edit"
-
-    fill_in "First name", with: "Stan"
-    fill_in "Last name", with: "Lee"
-      click_on "Update User"
-
-    expect(page).to have_content "Stan"
-    expect(page).to have_content "Lee"
-      click_on "Back"
-
-    expect(page).to have_content "Stan"
-    expect(page).to have_content "Lee"
-    expect(page).to have_content "example@example.com"
-      click_on "Edit"
-
-      click_on "Destroy"
-
-    expect(page).to have_no_content "Stan"
-    expect(page).to have_no_content "Lee"
-    expect(page).to have_no_content "example@example.com"
-
+      expect(page).to have_content "David"
+      expect(page).to have_content "Example"
+      expect(page).to have_content "example@example.com"
   end
 
+  scenario "User edits his username" do
+    visit users_path
+      click_on "Create User"
+      fill_in "First name", with: "David"
+      fill_in "Last name", with: "Example"
+      fill_in "Email address", with: "example@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_on "Create User"
+      expect(page).to have_content "David"
+      expect(page).to have_content "Example"
+      expect(page).to have_content "example@example.com"
+      click_on "Edit"
+      fill_in "First name", with: "Stan"
+      fill_in "Last name", with: "Lee"
+      click_on "Update User"
+      expect(page).to have_content "Stan"
+      expect(page).to have_content "Lee"
+      click_on "Back"
+      expect(page).to have_content "Stan"
+      expect(page).to have_content "Lee"
+      expect(page).to have_content "example@example.com"
+  end
+
+  scenario "User deletes a user" do
+    visit users_path
+      click_on "Create User"
+      fill_in "First name", with: "David"
+      fill_in "Last name", with: "Example"
+      fill_in "Email address", with: "example@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_on "Create User"
+      expect(page).to have_content "David"
+      expect(page).to have_content "Example"
+      expect(page).to have_content "example@example.com"
+  end
+
+  #     click_on "Destroy"
+  #
+  #   expect(page).to have_no_content "Stan"
+  #   expect(page).to have_no_content "Lee"
+  #   expect(page).to have_no_content "example@example.com"
+  #
+  # end
 end
-
-
-#
-#
-
-#       click_on "David"
-#
-#     expect(page).to have_content "David"
-#     expect(page).to have_content "Example"
-#     expect(page).to have_content "example@example.com"
-#       click_on "Edit"
-#
-#     expect(page).to have_content "First name"
-#     fill_in "First name", with: "Stan"
-#       click_on "Update User"
-#
-#     expect(page).to have_content "Stan"
-#       click_on "Back"
-#
-#     expect(page).to have_content "Stan"
-#     expect(page).to have_content "example@example.com"
-#       click_on "Sign Out"
-#     expect(page).to have_no_content "example@example.com"
-#
-#
-#   end
-#
-# end
