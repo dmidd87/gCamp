c05b7cfaba29953379b0325c411c76590a1daea9
Dmidd87 <dmidd87@gmail.com>
2014-11-08 14:34:39 -0700

    refactored projects test

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -5,25 +5,31 @@ feature "Projects" do
   scenario "User creates a project" do
     visit projects_path
       click_on "Create Project"
-
       fill_in "Name", with: "My awesome project!"
-        expect(page).to have_no_content "My awesome project!"
       click_on "Create Project"
-
-        expect(page).to have_content "My awesome project!"
-      click_on "Edit"
-
-      fill_in "Name", with: "My edited project!"
-      click_on "Update Project"
-
-        expect(page).to have_content "My edited project!"
-        expect(page).to have_no_content "My awesome project!"
-
-      click_on "Destroy"
-
-        expect(page). to have_no_content "My edited project!"
+      expect(page).to have_content "My awesome project!"
   end
 
+  scenario "User edits a project" do
+    visit projects_path
+      click_on "Create Project"
+      fill_in "Name", with: "My awesome project!"
+      click_on "Create Project"
+      expect(page).to have_content "My awesome project!"
+      click_on "Edit"
+      fill_in "Name", with: "My edited project!"
+      click_on "Update Project"
+      expect(page).to have_content "My edited project!"
+  end
 
+  scenario "User destroys a project" do
+    visit projects_path
+      click_on "Create Project"
+      fill_in "Name", with: "My awesome project!"
+      click_on "Create Project"
+      expect(page).to have_content "My awesome project!"
+      click_on "Destroy"
+      expect(page). to have_no_content "My edited project!"
+  end
 
 end
