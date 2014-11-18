a16ba9e52361960edffe7a513bcf42de22575221
Dmidd87 <dmidd87@gmail.com>
2014-11-18 15:39:51 -0700

    added  breadcrumbs and fixed some links on project show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   has_many :memberships
-  has_many :projects, through :memberships
+  has_many :projects, through: :memberships
 
   before_save { |user| user.email_address = email_address.downcase }
 
