799a0ca7c6238376bbcd56ad88a082c95b9ed91c
Dmidd87 <dmidd87@gmail.com>
2014-11-17 17:38:07 -0700

    Fixed user edits a task in project spec on tasks spec

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -18,15 +18,22 @@ feature "Tasks" do
   end
 
   scenario "User edits a task after making a project" do
-    visit tasks_path
+    visit projects_path
+      click_on "Create Project"
+      fill_in "Name", with: "New"
+      click_on "Create Project"
+      expect(page).to have_content "New"
+      click_on "0"
       click_on "Create Task"
-      fill_in "Description", with: "My awesome task!"
+      fill_in "Description", with: "party"
+      fill_in "Due", with: "12-12-2014"
       click_on "Create Task"
-      expect(page).to have_content "My awesome task!"
+      expect(page).to have_content "party"
+      expect(page).to have_content "2014-12-12"
       click_on "Edit"
-      fill_in "Description", with: "My edited task!"
+      fill_in "Description", with: "shindig"
       click_on "Update Task"
-      expect(page).to have_content "My edited task!"
+      expect(page).to have_content "shindig"
   end
 
   scenario "User deletes a task" do
