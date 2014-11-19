ba0fee91caea2db74881c56133230077d8370430
Dmidd87 <dmidd87@gmail.com>
2014-11-19 14:54:11 -0700

    Added some validations in the membership model

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,4 +2,8 @@ class Membership < ActiveRecord::Base
 
   belongs_to :project
   belongs_to :user
+
+  validates :user, :uniqueness => {scope: :project, message: "has already been added"}
+  validates :user, presence: true
+  validates :role, presence: true
 end
