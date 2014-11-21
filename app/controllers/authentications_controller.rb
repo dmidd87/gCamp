31b8b71042dac37f5d219c5c65f0b3ea7fb7d638
Dmidd87 <dmidd87@gmail.com>
2014-11-21 15:23:44 -0700

    Moved tasks create button to top right

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,7 +1,7 @@
 class AuthenticationsController < ApplicationController
 
   def create
-    @user = User.find_by_email(params[:authentication][:email].downcase)
+    @user = User.find_by_email_address(params[:email_address].downcase)
      if @user && @user.authenticate(params[:password])
        session[:user_id] = @user.id
        redirect_to root_path
