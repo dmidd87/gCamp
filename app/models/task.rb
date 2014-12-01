5cffff114d27790cc08158985ef0380dca22c62a
Dmidd87 <dmidd87@gmail.com>
2014-12-01 15:37:17 -0700

    Added date validations to tasks model and fixed empty date formatting issue with tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,14 @@
 class Task < ActiveRecord::Base
 
+  belongs_to :project
   has_many :comments, dependent: :destroy
-  validates :description, presence:true
 
+  validates :description, presence:true
+  validate :due_date_cannot_be_in_the_past, on: :create
+
+  def due_date_cannot_be_in_the_past
+    if due.present? && due < Date.today
+      errors.add(:due, "can't be in the past")
+    end
+  end
 end
