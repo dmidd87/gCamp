cafd35a9e6e7b10ccfdd772c95a6d473cf289e5d
Dmidd87 <dmidd87@gmail.com>
2014-12-02 13:38:15 -0700

    Added something in js we need for dropdown and wrote task date spec in model spec

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+  describe 'Due' do
+
+    it 'validates the presence of a description' do
+      task = Task.new
+      task.valid?
+      expect(task.errors[:description].present?).to eq(true)
+      task.description = 'test'
+      task.valid?
+      expect(task.errors[:description].present?).to eq(false)
+    end
+
+    it 'validates the presence of a valid task date' do
+      task = Task.new
+      task.valid?
+      expect(task.errors[:description].present?).to eq(true)
+      task.description = 'test'
+      task.valid?
+      expect(task.errors[:description].present?).to eq(false)
+      task.due = Date.today-7
+      task.valid?
+      expect(task.errors[:due].present?).to eq(true)
+      task.due = Date.today+7
+      task.valid?
+      expect(task.errors[:due].present?).to eq(false)
+    end
+
+    it 'validates that task date is valid in edit' do
+      task = Task.create!(description: 'task', due: Date.today+7)
+      task.due = Date.today-7
+      task.valid?
+      expect(task.errors[:due].present?).to eq(false)
+      task.due = Date.today+7
+      task.valid?
+      expect(task.errors[:due].present?).to eq(false)
+    end
+end
