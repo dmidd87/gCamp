95fbd484131a6121c7f4f760cf3b145b3809aae9
Dmidd87 <dmidd87@gmail.com>
2014-12-03 09:20:40 -0700

    Added spec for redirecting to projects index

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 feature "Projects" do
 
+  background do
+    @password = "password"
+    @user = User.new(
+      :first_name => "David",
+      :last_name => "Example",
+      :email_address => "david@example.com",
+      :password => @password,
+      :password_confirmation => @password
+    )
+  end
+
   scenario "User creates a project" do
     visit projects_path
       click_on "Create Project"
@@ -37,5 +48,22 @@ feature "Projects" do
     click_on "Create Project"
     click_on "Create Project"
     expect(page). to have_content "Name can't be blank"
+  end
+
+  scenario "User creates a project and is automatically listed as owner" do
+      register(@user,@password)
+      expect(page).to have_content @user.first_name
+      click_on "Sign Out"
+      click_on "Sign In"
+      fill_in "Email", with: "david@example.com"
+      fill_in "Password", with: "password"
+      click_on "Enter"
+      expect(page).to have_content "Create Project"
+      click_on "Create Project"
+      fill_in "Name", with: "Assignment"
+      click_on "Create Project"
+      click_on "1 Member"
+      expect(page).to have_content "David Example"
+      expect(page).to have_content "Owner"
   end
 end
