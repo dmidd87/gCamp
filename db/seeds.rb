daa781c24197a128beab69ae4f565208fba179ff
Dmidd87 <dmidd87@gmail.com>
2014-12-04 09:08:37 -0700

    Fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,28 @@ Membership.delete_all
 admin = User.create!(
   first_name: 'Admin',
   last_name: 'User',
-  email: 'admin@example.com',
+  email_address: 'admin@example.com',
   password: 'password'
 )
 
 owner = User.create!(
   first_name: 'Owner',
   last_name: 'User',
-  email: 'owner@example.com',
+  email_address: 'owner@example.com',
   password: 'password'
 )
 
 member = User.create!(
   first_name: 'Member',
   last_name: 'User',
-  email: 'member@example.com',
+  email_address: 'member@example.com',
   password: 'password'
 )
 
 user = User.create!(
   first_name: 'Basic',
   last_name: 'User',
-  email: 'user@example.com',
+  email_address: 'user@example.com',
   password: 'password'
 )
 
@@ -52,27 +52,27 @@ Membership.create!(
 task1 = Task.create!(
   description: "Write 3 comments",
   project: multiple_owners,
-  due_date: 4.days.from_now
+  due: 4.days.from_now
 )
 
 task2 = Task.create!(
   description: "Write 1 comment",
   project: multiple_owners,
-  due_date: 5.days.from_now
+  due: 5.days.from_now
 )
 
 3.times do
   Comment.create!(
     task: task1,
     user: owner,
-    body: Faker::Lorem.sentence
+    comment: Faker::Lorem.sentence
   )
 end
 
 Comment.create!(
   task: task2,
   user: member,
-  body: Faker::Lorem.sentence
+  comment: Faker::Lorem.sentence
 )
 
 single_owner = Project.create!(name: 'Single Owner')
