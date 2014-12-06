eaa3be610bc56a7cbe0c32f4246f210aa723ee64
Dmidd87 <dmidd87@gmail.com>
2014-12-06 13:01:09 -0700

    added pending test for 404 in tasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -17,5 +17,20 @@ describe TasksController do
       get :index, project_id: project.id
       expect(response).to be_success
     end
+
+    it "doesn't displays tasks to users who are owners" do
+      pending
+      user = create_user
+      project = create_project
+      task = create_task(project)
+      membership = create_member(user,project)
+      session[:user_id] = user.id
+      get :index, project_id: project.id
+      expect(response.status).to eq(200)
+      user2 = create_user2
+      session[:user2_id] = user.id
+      expect(response.status).to eq(404)
+
+    end
   end
 end
