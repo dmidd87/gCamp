7daf5ea85d16213876166d5e342f6c24bfb1c963
Dmidd87 <dmidd87@gmail.com>
2014-12-06 12:39:38 -0700

    Test for users being able to see their own tasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -4,8 +4,18 @@ describe TasksController do
   describe "index" do
     it "redirects visitors to login" do
       project = create_project
-      get :index, {project_id: project.id}
+      get :index, project_id: project.id
       expect(response).to redirect_to(signin_path)
+    end
+
+    it "displays tasks to users who are members" do
+      user = create_user
+      project = create_project
+      task = create_task(project)
+      membership = create_member(user,project)
+      session[:user_id] = user.id
+      get :index, project_id: project.id
+      expect(response).to be_success
     end
   end
 end
--- spec/support/object_creation_methods.rb
+++ spec/support/object_creation_methods.rb
@@ -1,71 +1,74 @@
-  def new_project(overrides = {})
-    defaults = { name: Faker::Company.name }
-    Project.new(defaults.merge(overrides))
-  end
-
-  def create_project(overrides = {})
-    new_project(overrides).tap(&:save!)
-  end
-
-  def create_user(overrides = {})
-    defaults = {
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      email_address: Faker::Internet.email,
-      password: 'password',
-    }
-
-    User.create(defaults.merge(overrides))
-  end
-
-  def create_membership(overrides = {})
-    defaults = {
-      user: create_user,
-      project: create_project,
-      role: 'Member',
-    }
-    Membership.create!(defaults.merge(overrides))
-  end
-
-  def new_task(overrides = {})
-    defaults = {
-      project: create_project,
-      description: Faker::Lorem.sentence,
-      due_date: '01/01/2999',
-    }
-    Task.new(defaults.merge(overrides))
-  end
-
-
-  def create_task(overrides = {})
-    defaults = {
-      project: create_project,
-      description: Faker::Lorem.sentence,
-      due_date: '01/01/2999',
-    }
-    Task.create!(defaults.merge(overrides))
-  end
-
-  def create_comment(overrides = {})
-    defaults = {
-      task: create_task,
-      copy: Faker::Lorem.sentence,
-    }
-    Comment.create!(defaults.merge(overrides))
-  end
-
-  def create_session
-    create_user
-    session[:user_id] = create_user.id
-  end
-
-  def create_admin
-    User.create!(
+def create_admin
+  User.create!(
     first_name: "Test",
     last_name: "Admin",
-    email: "admin@test.com",
+    email_address: "admin@test.com",
     password: "password",
     password_confirmation: "password",
     admin: true,
-    )
-  end
+  )
+end
+
+def create_user
+  User.create!(
+    first_name: "Test",
+    last_name: "User",
+    email_address: "test@test.com",
+    password: "password",
+    password_confirmation: "password",
+  )
+end
+
+def create_user2
+  User.create!(
+  first_name: "Test2",
+  last_name: "User",
+  email_address: "test2@test.com",
+  password: "password",
+  password_confirmation: "password",
+  )
+end
+
+def create_project
+  Project.create!(
+    name: "Test Project",
+  )
+end
+
+def create_project2
+  Project.create!(
+  name: "Test Project 2",
+  )
+end
+
+def create_owner(user, project)
+  Membership.create!(
+    role: "owner",
+    user_id: user.id,
+    project_id: project.id,
+  )
+end
+
+def create_member(user, project)
+  Membership.create!(
+  role: "member",
+  user_id: user.id,
+  project_id: project.id,
+  )
+end
+
+def create_task(project)
+  Task.create!(
+  description: "Test Task",
+  project_id: project.id,
+  due: "01/01/2999"
+  )
+end
+
+def create_comment(user, task)
+  Comment.create!(
+    description: "Test Comment",
+    user_id: user.id,
+    task_id: task.id,
+  )
+end
