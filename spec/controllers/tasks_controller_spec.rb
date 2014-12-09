61b7c21b2841aa1533c43831ecc2b9ea553c3d30
Dmidd87 <dmidd87@gmail.com>
2014-12-09 13:55:12 -0700

    Task controller spec working

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -9,7 +9,7 @@ describe TasksController do
     end
 
     it "displays tasks to users who are members" do
-      pending
+      User.delete_all
       user = create_user
       project = create_project
       task = create_task(project)
@@ -19,26 +19,38 @@ describe TasksController do
       expect(response).to be_success
     end
 
-    it "doesn't displays tasks to users who are owners" do
-      pending
+    it "displays tasks to owners who are members" do
+      User.delete_all
       user = create_user
       project = create_project
       task = create_task(project)
-      membership = create_member(user,project)
+      membership = create_owner(user,project)
+      session[:user_id] = user.id
+      get :index, project_id: project.id
+      expect(response).to be_success
+    end
+
+    it "doesn't displays tasks to users who aren't members or owners" do
+      User.delete_all
+      user = create_user
+      project = create_project
+      task = create_task(project)
+      membership = create_member(user, project)
       session[:user_id] = user.id
       get :index, project_id: project.id
       expect(response.status).to eq(200)
       user2 = create_user2
-      session[:user2_id] = user.id
+      session[:user_id] = user2.id
+      get :index, project_id: project.id
       expect(response.status).to eq(404)
     end
 
     it "redirects to tasks index after a user creates a project" do
-      pending
+      User.delete_all
       user = create_user
       project = create_project
       get :index, project_id: project.id
-      expect(response.status).to eq(200)
+      expect(response.status).to eq(302)
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,27 +1,60 @@
 require 'rails_helper'
 
 describe UsersController do
-  describe "Users" do
+  describe "#index" do
 
-    it "redirects visitor to login page if they try to access users" do
+  before do
+    User.delete_all
+  end
+
+  it "redirects visitor to login page if they try to access users" do
       get :index
       expect(response).to redirect_to(signin_path)
     end
   end
 
-    it "User can't any other other users but themselves" do
-      user1 = User.create!(first_name: "David", last_name: "Example", email_address: "david@example.com", password: "password")
-      user2 = User.create!(first_name: "Bob", last_name: "Hotdog", email_address: "bob@example.com", password: "password")
-      session[:user_id] = user1.id
-      get :edit, id: user2.id
-      expect(response.status).to eq(404)
+  it "User can't edit any other other users but themselves" do
+    user1 = User.create!(first_name: "David", last_name: "Example", email_address: "david@example.com", password: "password")
+    user2 = User.create!(first_name: "Bob", last_name: "Hotdog", email_address: "bob@example.com", password: "password")
+    session[:user_id] = user1.id
+    get :edit, id: user2.id
+    expect(response.status).to eq(404)
+  end
+
+  it "User can edit themselves" do
+    user1 = User.create!(first_name: "David", last_name: "Example", email_address: "david@example.com", password: "password")
+    user2 = User.create!(first_name: "Bob", last_name: "Hotdog", email_address: "bob@example.com", password: "password")
+    session[:user_id] = user1.id
+    get :edit, id: user1.id
+    expect(response.status).to eq(200)
+  end
+
+describe "create" do
+  
+  before do
+    User.delete_all
+  end
+
+  it "allows admin to create a user" do
+    admin = create_admin
+    session[:user_id] = admin.id
+    post :create, user: {first_name: "Test",
+                        last_name: "User",
+                        email: "test@test.com",
+                        password: "password",
+                        password_confirmation: "password"}
+    expect(User.count == 2)
     end
 
-    it "User can edit themselves" do
-      user1 = User.create!(first_name: "David", last_name: "Example", email_address: "david@example.com", password: "password")
-      user2 = User.create!(first_name: "Bob", last_name: "Hotdog", email_address: "bob@example.com", password: "password")
-      session[:user_id] = user1.id
-      get :edit, id: user1.id
-      expect(response.status).to eq(200)
+    it "allows user to create a user" do
+      user = create_user
+      session[:user_id] = user.id
+      post :create, user: {first_name: "Test",
+                          last_name: "User",
+                          email: "test22@test.com",
+                          password: "password",
+                          password_confirmation: "password"}
+      expect(User.count == 2)
     end
+  end
 end
