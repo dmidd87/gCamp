e05824118bd2f38fe3c180cfbcb1dde8f4212f7c
Dmidd87 <dmidd87@gmail.com>
2015-01-23 14:15:43 -0700

    fixed quotes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,16 @@ class PagesController < PublicController
   def index
 
     quote1 = Quote.new
-    quote1.text = "Failure is not an option"
-    quote1.author = "-Bob"
+    quote1.text = "You have to learn the rules of the game.  And then you have to play better than everyone else."
+    quote1.author = "-Albert Einstein"
 
     quote2 = Quote.new
-    quote2.text = "Success please"
-    quote2.author = "-Me"
+    quote2.text = "Do not take life too seriously.  You will never get out alive."
+    quote2.author = "-Elbert Hubbard"
 
     quote3 = Quote.new
-    quote3.text = "Win everything!"
-    quote3.author = "-Stan"
+    quote3.text = "It does not matter how slowly you go as long as you don't stop"
+    quote3.author = "-Confucious"
 
     @quotes = [quote1, quote2, quote3]
   end
